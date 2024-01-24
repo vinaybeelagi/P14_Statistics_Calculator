3b8b206521da265bf33a228a581b54a5c6ddbade
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-24 15:38:18 +0530

    getMedian Function:Calculates the median of an array of numbers.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 //getMean Function
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+//getMedian Function:Calculates the median of an array of numbers.
+const getMedian = (array) => {
+  const sorted = array.slice().sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+}
+
+
 
